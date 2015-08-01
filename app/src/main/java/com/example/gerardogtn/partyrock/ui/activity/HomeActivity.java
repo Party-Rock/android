1be829277f467116f564fc4ee21158ae75f98fa8
Gerardo Teruel <gerardogtn@gmail.com>
2015-08-01 14:38:05 -0500

    :rocket: Add back arrow

--- app/src/main/java/com/example/gerardogtn/partyrock/ui/activity/HomeActivity.java
+++ app/src/main/java/com/example/gerardogtn/partyrock/ui/activity/HomeActivity.java
@@ -1,17 +1,38 @@
-package com.example.gerardogtn.partyrock;
+package com.example.gerardogtn.partyrock.ui.activity;
+
 
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.example.gerardogtn.partyrock.R;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
 
 public class HomeActivity extends AppCompatActivity {
+
+    @Bind(R.id.toolbar_home)
+    Toolbar mToolbar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        ButterKnife.bind(this);
+        setUpToolbar();
+    }
+
+    private void setUpToolbar() {
+        setSupportActionBar(mToolbar);
+        ActionBar actionBar = getSupportActionBar();
+        assert actionBar != null;
+        actionBar.setDisplayHomeAsUpEnabled(true);
     }
 
     @Override
