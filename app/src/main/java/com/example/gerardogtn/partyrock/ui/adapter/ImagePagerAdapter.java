5632f0852b0301598d746468f9d2c7586a935586
Gerardo Teruel <gerardogtn@gmail.com>
2015-08-01 22:39:47 -0500

    :rocket: Add ImagePagerAdapter

---
+++ app/src/main/java/com/example/gerardogtn/partyrock/ui/adapter/ImagePagerAdapter.java
@@ -0,0 +1,41 @@
+package com.example.gerardogtn.partyrock.ui.adapter;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import com.example.gerardogtn.partyrock.ui.fragment.ImageFragment;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by gerardogtn on 8/1/15.
+ */
+public class ImagePagerAdapter extends FragmentPagerAdapter {
+
+    private List<Fragment> mFragments;
+    private int mImageResource;
+
+    public ImagePagerAdapter(FragmentManager fm) {
+        super(fm);
+        mFragments = new ArrayList<>();
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return mFragments.get(position);
+    }
+
+    @Override
+    public int getCount() {
+        return mFragments.size();
+    }
+
+    // REQUIRES: None
+    // MODIFIES: this
+    // EFFECTS: Adds fragment to this.mFragments
+    public void addFragment(Fragment fragment){
+        mFragments.add(fragment);
+    }
+}
