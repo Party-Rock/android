100acf76a4d18382a1f9a7f833dc0a07225c6d35
Gerardo Teruel <gerardogtn@gmail.com>
2015-08-05 20:07:11 -0500

    :rocket: Add implementation to get user's location

--- app/src/main/java/com/example/gerardogtn/partyrock/ui/activity/HomeActivity.java
+++ app/src/main/java/com/example/gerardogtn/partyrock/ui/activity/HomeActivity.java
@@ -1,29 +1,60 @@
 package com.example.gerardogtn.partyrock.ui.activity;
 
+import android.location.Location;
 import android.os.Bundle;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.style.TtsSpan;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.gerardogtn.partyrock.R;
 import com.example.gerardogtn.partyrock.ui.fragment.HomeListFragment;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
+
+import java.text.BreakIterator;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
 
-public class HomeActivity extends AppCompatActivity implements HomeListFragment.OnVenueSelectedListener {
+public class HomeActivity extends AppCompatActivity implements HomeListFragment.OnVenueSelectedListener,
+        GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener {
+
+    public static final String TAG = HomeActivity.class.getSimpleName();
+
+    private GoogleApiClient mGoogleApiClient;
+    private Location mLastLocation;
+
+    // Bool to track whether the app is already resolving an error
+    private boolean mResolvingError = false;
 
     @Bind(R.id.toolbar_home)
     Toolbar mToolbar;
+
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if (!mResolvingError) {  // more about this later
+            mGoogleApiClient.connect();
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        buildGoogleApiClient();
         ButterKnife.bind(this);
         setUpToolbar();
         addHomeListFragment();
@@ -31,7 +62,6 @@ public class HomeActivity extends AppCompatActivity implements HomeListFragment.
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_home, menu);
         return true;
     }
@@ -39,6 +69,12 @@ public class HomeActivity extends AppCompatActivity implements HomeListFragment.
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onStop() {
+        mGoogleApiClient.disconnect();
+        super.onStop();
     }
 
     // REQUIRES: None.
@@ -51,7 +87,7 @@ public class HomeActivity extends AppCompatActivity implements HomeListFragment.
     // REQUIRES: None.
     // MODIFIES: this.
     // EFFECTS:  Draws back arrow in toolbar.
-    private void drawBackArrow(){
+    private void drawBackArrow() {
         ActionBar actionBar = getSupportActionBar();
         assert actionBar != null;
         actionBar.setDisplayHomeAsUpEnabled(true);
@@ -67,8 +103,46 @@ public class HomeActivity extends AppCompatActivity implements HomeListFragment.
         tm.commit();
     }
 
+    // REQUIRES: None.
+    // MODIFIES: this.
+    // EFFECTS: Builds the API for Google's LocationServices
+    protected synchronized void buildGoogleApiClient() {
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+    }
+
     @Override
     public void onVenueSelected(String name, int capacity, int price, int distance) {
 
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+        Log.i(TAG, "onConnected executed.");
+        mLastLocation = LocationServices.FusedLocationApi.getLastLocation(
+                mGoogleApiClient);
+        if (mLastLocation != null) {
+            String position = "Lat: " +
+                    String.valueOf(mLastLocation.getLatitude()) +
+                    "Lon: " +
+                    String.valueOf(mLastLocation.getLongitude());
+
+            Toast.makeText(this, position, Toast.LENGTH_LONG).show();
+        } else {
+            Log.e(TAG, "Location was null.");
+        }
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        Toast.makeText(this, "Failed to get location", Toast.LENGTH_LONG).show();
     }
 }
