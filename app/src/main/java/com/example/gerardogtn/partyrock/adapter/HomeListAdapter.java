84ffb5d20b83b5a2ad0a0a0b72515ee2c6c93cf4
emirua <emirua@hotmail.com>
2015-08-10 01:52:45 -0500

    :rocket: Add Date picker interaction

---
+++ app/src/main/java/com/example/gerardogtn/partyrock/adapter/HomeListAdapter.java
@@ -0,0 +1,145 @@
+package com.example.gerardogtn.partyrock.adapter;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.v4.view.ViewPager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.gerardogtn.partyrock.R;
+import com.example.gerardogtn.partyrock.data.Venue;
+import com.example.gerardogtn.partyrock.service.VenueEvent;
+import com.example.gerardogtn.partyrock.ui.activity.DetailActivity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import de.greenrobot.event.EventBus;
+
+/**
+ * Created by gerardogtn on 8/1/15.
+ */
+public class HomeListAdapter extends RecyclerView.Adapter<HomeListAdapter.HomeListViewHolder>{
+
+    private Context mContext;
+    private List<Venue> mVenues = new ArrayList<>();
+    private LayoutInflater mInflater;
+    private OnVenueClickListener listener;
+
+    //Listener interface
+    public interface OnVenueClickListener {
+        void onVenueClick(int position);
+    }
+
+    // Method that allows the parent activity or fragment to define the listener
+    public void setOnItemClickListener(OnVenueClickListener listener) {
+        this.listener = listener;
+    }
+
+
+    public HomeListAdapter(Context context, List<Venue> venues) {
+        this.mVenues = venues;
+        mInflater = LayoutInflater.from(context);
+        mContext = context;
+    }
+
+
+    // REQUIRES: None.
+    // MODIFIES: None.
+    // EFFECTS: Returns a new inflated view for an item.
+    @Override
+    public HomeListViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View item = mInflater.inflate(R.layout.item_venue_list, parent, false);
+        return new HomeListViewHolder(item);
+    }
+
+
+    // REQUIRES: None.
+    // MODIFIES: None.
+    // EFFECTS:  Populates a view with the data of a Venue.
+    @Override
+    public void onBindViewHolder(HomeListViewHolder holder, int position) {
+        holder.setData(mVenues.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return mVenues.size();
+    }
+
+    public class HomeListViewHolder extends RecyclerView.ViewHolder {
+
+        @Bind(R.id.venue_images)
+        ViewPager mImages;
+
+        @Bind(R.id.txt_capacity)
+        TextView mCapacity;
+
+        @Bind(R.id.txt_distance)
+        TextView mDistance;
+
+        @Bind(R.id.txt_price)
+        TextView mPrice;
+
+        private List<String> mImageUrls;
+
+        public HomeListViewHolder(final View itemView) {
+            super(itemView);
+            ButterKnife.bind(this, itemView);
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (listener != null) {
+                        listener.onVenueClick(getLayoutPosition());
+                    }
+                }
+            });
+
+
+        }
+
+        // REQUIRES: None.
+        // MODIFIES: this.
+        // EFFECTS:  Represents and visualizes venue data with views.
+        public void setData(Venue venue) {
+            mCapacity.setText(""+ Integer.toString(venue.getmCapacity()));
+            mDistance.setText(Double.toString(venue.getmDistance())+"km.");
+            mPrice.setText("$"+Double.toString(venue.getmPrice()));
+            mImageUrls = venue.getImageUrls();
+            setUpViewPager(venue.getImageUrls());
+        }
+
+
+        // REQUIRES: None.
+        // MODIFIES: this.
+        // EFFECTS:  If the imageResource is invalid(-1) add a new ImageFragment with default Image.
+        //           Otherwise, add a new ImageFragment with the specified image resource.
+        private void setUpViewPager(List<String> imageUrls) {
+            ImagePagerAdapter adapter = new ImagePagerAdapter(mContext, imageUrls);
+            mImages.setAdapter(adapter);
+            adapter.setOnItemClickListener(new ImagePagerAdapter.OnVenueClickListener() {
+                @Override
+                public void onVenueClick() {
+                    VenueEvent clickedVenue = new VenueEvent(mVenues.get(getLayoutPosition()));
+                    EventBus.getDefault().postSticky(clickedVenue);
+                    Intent intent = new Intent(mContext, DetailActivity.class);
+                    mContext.startActivity(intent);
+                }
+            });
+//            mImages.setOnClickListener(new View.OnClickListener() {
+//                @Override
+//                public void onClick(View v) {
+//                    if (listener != null) {
+//                        listener.onVenueClick(itemView, getLayoutPosition());
+//                    }
+//                }
+//
+//            });
+        }
+    }
+}
---
+++ app/src/main/java/com/example/gerardogtn/partyrock/ui/fragment/ConfirmationFragment.java
@@ -0,0 +1,125 @@
+package com.example.gerardogtn.partyrock.ui.fragment;
+
+
+import android.app.DatePickerDialog;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.gerardogtn.partyrock.R;
+import com.example.gerardogtn.partyrock.data.Venue;
+import com.example.gerardogtn.partyrock.service.VenueEvent;
+import com.squareup.picasso.Picasso;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import de.greenrobot.event.EventBus;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class ConfirmationFragment extends Fragment {
+    @Bind(R.id.btn_rent)
+    Button rentButton;
+
+    @Bind(R.id.img_venue_main)
+    ImageView mMainVenueImage;
+
+    @Bind(R.id.txt_name)
+    TextView mNameText;
+    @Bind(R.id.txt_price)
+    TextView mPriceText;
+    @Bind(R.id.txt_capacity)
+    TextView mCapacityText;
+    @Bind(R.id.txt_venue_description)
+    TextView mVenueText;
+
+    @Bind(R.id.datePickerText)
+    TextView datePickerText;
+
+
+    private DatePicker mDatePicker;
+    private DatePickerDialog mDatePickerDialog;
+    private SimpleDateFormat mSimpleDateFormat;
+    public ConfirmationFragment() {
+        // Required empty public constructor
+    }
+    //EventBus method to receive Venue
+    public void onEvent(VenueEvent clickedVenue){
+        Venue venue = clickedVenue.getVenue();
+        Toast.makeText(getActivity(), "Venue received " + clickedVenue.getVenue().getmName(), Toast.LENGTH_SHORT).show();
+        setLayoutDetails(venue);
+
+
+    }
+
+    public void setLayoutDetails(Venue venue) {
+        Picasso.with(getActivity())
+                .load(venue.getImageUrls().get(0))
+                .error(R.mipmap.ic_launcher)
+                .into(mMainVenueImage);
+        mNameText.setText(getActivity().getString(R.string.venue) + ": " + venue.getmName());
+        mCapacityText.setText(getActivity().getString(R.string.capacity)+": " + Integer.toString(venue.getmCapacity()));
+        mPriceText.setText("$" + Double.toString(venue.getmPrice()));
+        mVenueText.setText(venue.toString());
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_confirmation, container, false);
+        ButterKnife.bind(this, view);
+        EventBus.getDefault().registerSticky(this);
+        rentButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getActivity(), "Venue rented!", Toast.LENGTH_SHORT).show();
+            }
+        });
+        setCalendar();
+        return view;
+    }
+
+    public void setCalendar() {
+        mSimpleDateFormat = new SimpleDateFormat("dd/MM/yyyy", Locale.US);
+
+        datePickerText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mDatePickerDialog.show();
+            }
+        });
+        Calendar newCalendar = Calendar.getInstance();
+        mDatePickerDialog = new DatePickerDialog(getActivity(), new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                Calendar newDate = Calendar.getInstance();
+                newDate.set(year, monthOfYear, dayOfMonth);
+                datePickerText.setText(mSimpleDateFormat.format(newDate.getTime()));
+            }
+        }, newCalendar.get(Calendar.YEAR),newCalendar.get(Calendar.MONTH), newCalendar.get(Calendar.DAY_OF_MONTH));
+        long currentTime = System.currentTimeMillis();
+        mDatePicker = mDatePickerDialog.getDatePicker();
+        mDatePicker.setMinDate(currentTime - 1000);
+        long dateMax = (currentTime/100) + currentTime;
+        mDatePicker.setMaxDate(dateMax);
+
+    }
+
+    @Override
+    public void onPause() {
+        EventBus.getDefault().unregister(this);
+        super.onPause();
+    }
+}
