a0f4455164476abf6b2d4253bccfd700e1cb543d
Gerardo Teruel <gerardogtn@gmail.com>
2015-08-18 12:25:29 -0500

    :sweat_smile: Fix bug in URL_VENUES

--- app/src/main/java/com/example/gerardogtn/partyrock/util/ApiConstants.java
+++ app/src/main/java/com/example/gerardogtn/partyrock/util/ApiConstants.java
@@ -4,12 +4,11 @@ package com.example.gerardogtn.partyrock.util;
  * Created by gerardogtn on 8/18/15.
  */
 public class ApiConstants {
-
     public static final String URL_BASE = "http://partyrock-49947.onmodulus.net";
 
     public static final String API_VERSION = "/v1";
 
     public static final String PARAM_VENUES = "/venues";
 
-    public static final String URL_ALL_VENUES = URL_BASE + API_VERSION + PARAM_VENUES;
+    public static final String URL_ALL_VENUES = API_VERSION + PARAM_VENUES;
 }
