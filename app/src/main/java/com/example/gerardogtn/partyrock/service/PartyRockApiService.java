8ebdfe706db4a709a63bd2d88139ed9b06f67083
Gerardo Teruel <gerardogtn@gmail.com>
2015-08-19 19:06:24 -0500

    :rocket: Add postReservation

--- app/src/main/java/com/example/gerardogtn/partyrock/service/PartyRockApiService.java
+++ app/src/main/java/com/example/gerardogtn/partyrock/service/PartyRockApiService.java
@@ -11,6 +11,7 @@ import retrofit.Callback;
 import retrofit.client.Response;
 import retrofit.http.Body;
 import retrofit.http.Field;
+import retrofit.http.FormUrlEncoded;
 import retrofit.http.GET;
 import retrofit.http.POST;
 
@@ -22,12 +23,19 @@ public interface PartyRockApiService {
     @GET(ApiConstants.URL_ALL_VENUES)
     void getAllVenues(Callback<List<Venue>> callback);
 
-    @POST(ApiConstants.URL_ALL_USERS)
-    void postUser(@Body User user, Callback<Response> responseCallback);
-
+    @FormUrlEncoded
     @POST(ApiConstants.URL_ALL_USERS)
     void postUser(@Field(ApiConstants.PARAM_FIRST_NAME) String firstName,
                   @Field(ApiConstants.PARAM_LAST_NAME) String lastName,
                   @Field(ApiConstants.PARAM_PASSWORD) String password,
                   Callback<Response> responseCallback);
+
+    @FormUrlEncoded
+    @POST(ApiConstants.URL_ALL_RESERVATIONS)
+    void postReservation(@Field(ApiConstants.PARAM_USER_ID) String userId,
+                         @Field(ApiConstants.PARAM_OWNER_ID) String ownerId,
+                         @Field(ApiConstants.PARAM_VENUE_ID) String venueId,
+                         @Field(ApiConstants.PARAM_RENTED_DATE) String dateIso,
+                         Callback<Response> responseCallback);
+
 }
